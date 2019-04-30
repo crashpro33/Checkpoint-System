3feee9e8876f627ebb177cea342cb465f10afeb3
Matthew King <32280053+crashpro33@users.noreply.github.com>
2019-04-30 18:58:11 -0500

    Update Checkpoint.cs

--- Checkpoint.cs
+++ Checkpoint.cs
@@ -4,41 +4,41 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
 
-    public HealthManager healthManager;
+	public HealthManager healthManager;
 
-    public Renderer rend;
+	public Renderer rend;
 
-    public Material checkOff;
-    public Material checkOn;
+	public Material checkOff;
+	public Material checkOn;
 
 	// Use this for initialization
 	void Start () {
-        healthManager = FindObjectOfType<HealthManager>();
+	healthManager = FindObjectOfType<HealthManager>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
-    public void CheckpointOn() {
-        Checkpoint[] checkpoints = FindObjectsOfType<Checkpoint>();
-        foreach(Checkpoint cp in checkpoints) {
-            cp.CheckpointOff();
-        }
-        rend.material = checkOn;
-    }
+	public void CheckpointOn() {
+		Checkpoint[] checkpoints = FindObjectsOfType<Checkpoint>();
+		foreach(Checkpoint cp in checkpoints) {
+	    		cp.CheckpointOff();
+		}
+		rend.material = checkOn;
+	}
 
-    public void CheckpointOff() {
-        rend.material = checkOff;
-    }
+	public void CheckpointOff() {
+		rend.material = checkOff;
+	}
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.tag.Equals("Player")) {
-            Vector3 spawn = transform.position;
-            spawn.y += 2;
-            healthManager.SetSpawnPoint(spawn);
-            CheckpointOn();
-        }
-    }
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag.Equals("Player")) {
+	    		Vector3 spawn = transform.position;
+	    		spawn.y += 2;
+	    		healthManager.SetSpawnPoint(spawn);
+	    		CheckpointOn();
+		}
+	}
 }
